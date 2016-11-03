953e9ccfad8da4d5f4308b66df4dd7fd4ffb1ffc
Yoti <realyoti@gmail.com>
2016-11-03 22:13:52 +0300

    v0.6

---
+++ main.c
@@ -0,0 +1,276 @@
+#include <psp2/display.h>
+#include <psp2/io/fcntl.h>
+#include <psp2/kernel/processmgr.h>
+
+#include <stdio.h> // sprintf()
+#include <psp2/ctrl.h> // sceCtrl*()
+
+#include "graphics.h"
+
+#define VER_MAJOR 0
+#define VER_MINOR 6
+
+#define printf psvDebugScreenPrintf
+
+int _vshSblAimgrGetConsoleId(char CID[32]);
+
+/*
+	Model: FatWF, SKU: PCH-1000, MoBo: IRS-002;
+	Model: Fat3G, SKU: PCH-1100, MoBo: IRS-002;
+	Model: Slim, SKU: PCH-2000, MoBo: USS-1001;
+	Model: TV, SKU: VTE-1000, MoBo: DOL-1001.
+
+	No diff between FatWF and Fat3G.
+	No diff between Vita TV (Asian) and PSTV (Western).
+*/
+
+SceCtrlData pad;
+
+void ExitCross(char*text)
+{
+	printf("%s, press X to exit...\n", text);
+	do
+	{
+		sceCtrlReadBufferPositive(0, &pad, 1);
+		sceKernelDelayThread(0.05*1000*1000);
+	}
+	while(!(pad.buttons & SCE_CTRL_CROSS));
+	sceKernelExitProcess(0);
+}
+
+void ExitError(char*text, int delay, int error)
+{
+	printf(text, error);
+	sceKernelDelayThread(delay*1000*1000);
+	sceKernelExitProcess(0);
+}
+
+int WriteFile(char*file, void*buf, int size)
+{
+	sceIoRemove(file);
+	SceUID fd = sceIoOpen(file, SCE_O_WRONLY | SCE_O_CREAT | SCE_O_TRUNC, 0777);
+	if (fd < 0)
+		return fd;
+
+	int written = sceIoWrite(fd, buf, size);
+	sceIoClose(fd);
+
+	return written;
+}
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int paranoid = 0;
+	char idps_buffer[32];
+	unsigned char idps_text_char_tmp[1];
+	unsigned char idps_text_char_1st[1];
+	unsigned char idps_text_char_2nd[1];
+	char idps_text_buffer[32] = "";
+
+	sceKernelDelayThread(1000000);
+	sceCtrlReadBufferPositive(0, &pad, 1);
+	sceKernelDelayThread(1000000);
+	if (pad.buttons & SCE_CTRL_LTRIGGER)
+		paranoid = 1;
+
+	psvDebugScreenInit();
+	psvDebugScreenClear(0);
+	printf("PSV IDPS Dumper v%i.%i by Yoti\nbased on VitaCID by Major_Tom\n\n", VER_MAJOR, VER_MINOR);
+
+	_vshSblAimgrGetConsoleId(idps_buffer);
+
+	printf(" Your IDPS is: ");
+	if (paranoid == 1)
+	{
+		for (i=0; i<0x08; i++)
+		{
+			if (i == 0x04)
+				psvDebugScreenSetFgColor(0xFF0000FF);
+			else if (i == 0x06)
+				psvDebugScreenSetFgColor(0xFF0000FF);
+			else if (i == 0x07)
+				psvDebugScreenSetFgColor(0xFF00FF00);
+			else if (i == 0x05)
+				psvDebugScreenSetFgColor(0xFFFF0000);
+			else
+				psvDebugScreenSetFgColor(0xFFFFFFFF);
+			printf("%02X", (u8)idps_buffer[i]);
+		}
+		for (i=0; i<0x08; i++)
+		{
+			psvDebugScreenSetFgColor(0xFFFFFFFF);
+			printf("XX");
+		}
+	}
+	else
+	{
+		for (i=0; i<0x10; i++)
+		{
+			if (i == 0x04)
+				psvDebugScreenSetFgColor(0xFF0000FF);
+			else if (i == 0x06)
+				psvDebugScreenSetFgColor(0xFF0000FF);
+			else if (i == 0x07)
+				psvDebugScreenSetFgColor(0xFF00FF00);
+			else if (i == 0x05)
+				psvDebugScreenSetFgColor(0xFFFF0000);
+			else
+				psvDebugScreenSetFgColor(0xFFFFFFFF);
+			printf("%02X", (u8)idps_buffer[i]);
+		}
+	}
+	printf("\n\n");
+
+	printf(" It seems that you are using ");
+	psvDebugScreenSetFgColor(0xFF0000FF);
+	if (idps_buffer[0x04] == 0x00)
+		printf("PlayStation Portable");
+	else if (idps_buffer[0x04] == 0x01) // psv, vtv/pstv
+	{
+		if (idps_buffer[0x06] == 0x00)
+			printf("PlayStation Vita"); // fatWF/fat3G, slim
+		else if (idps_buffer[0x06] == 0x02)
+			printf("PlayStation/Vita TV"); // vtv, pstv
+		else
+			printf("Unknown Vita 0x%02X", idps_buffer[0x06]);
+	}
+	else
+		printf("Unknown PS 0x%02X", idps_buffer[0x04]);
+	psvDebugScreenSetFgColor(0xFFFFFFFF);
+	printf("\n");
+
+	printf(" Your motherboard is ");
+	psvDebugScreenSetFgColor(0xFF00FF00);
+	if (idps_buffer[0x06] == 0x00) // portable
+	{
+		switch(idps_buffer[0x07])
+		{
+			case 0x01:
+				printf("TA-079/081 (PSP-1000)");
+				break;
+			case 0x02:
+				printf("TA-082/086 (PSP-1000)");
+				break;
+			case 0x03:
+				printf("TA-085/088 (PSP-2000)");
+				break;
+			case 0x04:
+				printf("TA-090v2/092 (PSP-3000)");
+				break;
+			case 0x05:
+				printf("TA-091 (PSP-N1000)");
+				break;
+			case 0x06:
+				printf("TA-093 (PSP-3000)");
+				break;
+			//case 0x07:
+			//	printf("???");
+			//	break;
+			case 0x08:
+				printf("TA-095/095v2 (PSP-3000)");
+				break;
+			case 0x09:
+				printf("TA-096/097 (PSP-E1000)");
+				break;
+			case 0x10:
+				printf("IRS-002 (PCH-1000/1100)");
+				break;
+			case 0x14:
+				printf("USS-1001 (PCH-2000)");
+				break;
+			default:
+				printf("Unknown MoBo 0x%02X", idps_buffer[0x07]);
+				break;
+		}
+	}
+	else if (idps_buffer[0x06] == 0x02) // home system
+	{
+		switch(idps_buffer[0x07])
+		{
+			case 0x01:
+				printf("DOL-1001 (VTE-1000)");
+				break;
+			default:
+				printf("Unknown MoBo 0x%02X", idps_buffer[0x07]);
+				break;
+		}
+	}
+	else
+		printf("Unknown type 0x%02X", idps_buffer[0x06]);
+	psvDebugScreenSetFgColor(0xFFFFFFFF);
+	printf("\n");
+
+	printf(" And your region is ");
+	psvDebugScreenSetFgColor(0xFFFF0000);
+	switch(idps_buffer[0x05])
+	{
+		case 0x03:
+			printf("Japan");
+			break;
+		case 0x04:
+			printf("North America");
+			break;
+		case 0x05:
+			printf("Europe/East/Africa");
+			break;
+		case 0x06:
+			printf("Korea");
+			break;
+		case 0x07: // PCH-xx03 VTE-1016
+			printf("Great Britain/United Kingdom");
+			break;
+		case 0x08:
+			printf("Mexica/Latin America");
+			break;
+		case 0x09:
+			printf("Australia/New Zeland");
+			break;
+		case 0x0A:
+			printf("Hong Kong/Singapore");
+			break;
+		case 0x0B:
+			printf("Taiwan");
+			break;
+		case 0x0C:
+			printf("Russia");
+			break;
+		case 0x0D:
+			printf("China");
+			break;
+		default:
+			printf("Unknown region 0x%02X", idps_buffer[0x05]);
+			break;
+	}
+	psvDebugScreenSetFgColor(0xFFFFFFFF);
+	printf("\n\n");
+
+	printf(" https://github.com/Yoti/psv_idpsdump/\n");
+
+	// binary
+	WriteFile("ux0:data/idps.bin", idps_buffer, 16);
+
+	// text
+	for (i=0; i<0x10; i++)
+	{
+		idps_text_char_tmp[1]=idps_buffer[i];
+		idps_text_char_1st[1]=(idps_text_char_tmp[1] & 0xf0) >> 4;
+		idps_text_char_2nd[1]=(idps_text_char_tmp[1] & 0x0f);
+
+		// 1st half of byte
+		if (idps_text_char_1st[1] < 0xA) // digit
+			sprintf(idps_text_buffer, "%s%02X", idps_text_buffer, idps_text_char_1st[1]+0x30);
+		else // char
+			sprintf(idps_text_buffer, "%s%02X", idps_text_buffer, idps_text_char_1st[1]+0x37);
+
+		// 2nd half of byte
+		if (idps_text_char_2nd[1] < 0xA) // digit
+			sprintf(idps_text_buffer, "%s%02X", idps_text_buffer, idps_text_char_2nd[1]+0x30);
+		else // char
+			sprintf(idps_text_buffer, "%s%02X", idps_text_buffer, idps_text_char_2nd[1]+0x37);
+	}
+	WriteFile("ux0:data/idps.txt", idps_text_buffer, 32);
+
+	ExitCross("\nDone");
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,37 @@
+TITLE_ID = YPID00001
+TARGET   = PSV_IDPS_Dumper_v0.6
+OBJS     = main.o graphics.o font.o
+
+LIBS = -lSceKernel_stub -lSceVshBridge_stub -lSceDisplay_stub -lSceCtrl_stub
+
+PREFIX  = arm-vita-eabi
+CC      = $(PREFIX)-gcc
+CFLAGS  = -Wl,-q -Wall -O3
+ASFLAGS = $(CFLAGS)
+
+all: $(TARGET).vpk
+
+%.vpk: eboot.bin
+	vita-mksfoex -d PARENTAL_LEVEL=1 -s APP_VER=00.60 -s TITLE_ID=$(TITLE_ID) "$(TARGET)" param.sfo
+	vita-pack-vpk -s param.sfo -b eboot.bin $@
+
+eboot.bin: $(TARGET).velf
+	vita-make-fself $< $@
+
+%.velf: %.elf
+	vita-elf-create $< $@
+
+$(TARGET).elf: $(OBJS)
+	$(CC) $(CFLAGS) $^ $(LIBS) -o $@
+
+clean:
+	@rm -rf $(TARGET).vpk $(TARGET).velf $(TARGET).elf $(OBJS) \
+		eboot.bin param.sfo
+
+vpksend: $(TARGET).vpk
+	curl -T $(TARGET).vpk ftp://$(PSVITAIP):1337/ux0:/
+	@echo "Sent."
+
+send: eboot.bin
+	curl -T eboot.bin ftp://$(PSVITAIP):1337/ux0:/app/$(TITLE_ID)/
+	@echo "Sent."
